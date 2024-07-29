4b72dca2e9ed12d6617d3231f62d01c96441f8e8
Soumily Maji <majisoumily14@gmail.com>
2024-07-28 23:55:49 -0700

    added more integration, just needs to fit website format

--- app.py
+++ app.py
@@ -31,6 +31,11 @@ serums_path = "serums.txt"
 moisturizers_path = "moisturizers.txt"
 toners_path = "toners.txt"
 
+cleanser_df = read_skincare_data(cleanser_path)
+toner_df = read_skincare_data(toners_path)
+serums_df = read_skincare_data(serums_path)
+moisturizers_df = read_skincare_data(moisturizers_path)
+
 def filter_by_feature(df, feature):
     return df[df['Features'].apply(lambda x: feature in x)]
 
@@ -38,19 +43,65 @@ def filter_out_features(df, features_to_exclude):
     return df[df['Features'].apply(lambda x: not any(feature in x for feature in features_to_exclude))]
 
 @app.route('/')
-def index():
+def right_sidebar():
     return render_template('right-sidebar.html')
 
-@app.route('/filter', methods=['POST'])
-def filter_data():
-    all_features = request.form.getlist('features')
-    print(all_features)
-    #file_path = 'cleansers.txt'
-   # df = read_skincare_data(file_path)
-    #filtered_df = filter_by_feature(all_features)
-    #result_html = filtered_df.to_html()
-    
-    #return result_html
-index()
-filter_data()
+@app.route('/filter', methods=['GET','POST'])
+def filter():
+    if request.method == "POST":
+        skin_type = request.form.get('skin-type')
+        weather = request.form.get('weather')
+        skin_goals = request.form.getlist('skin-goals')
+        sensitivity = request.form.get('sensitivity')
+        acne = request.form.get('acne')
+        uv_protection = request.form.get('uv')
+        parabens = request.form.get('paraben')
+        allergies = request.form.get('allergies')
 
+        include = []
+        exclude = []
+
+        if skin_type and skin_type == 'Good for Dry Skin':
+            include.append('Good for Dry Skin')
+            exclude.append('Bad for Dry Skin')
+        if skin_type and skin_type == 'Good for Oily Skin':
+            include.append('Good for Oily Skin')
+            exclude.append('Bad for Oily Skin')
+
+        if acne and acne == 'severe':
+            include.append('Promotes Wound Healing')
+            include.append('Acne-Fighting')
+        if acne and acne == 'Fungal Acne Trigger':
+            exclude.append('Fungal Acne Trigger')
+
+        if uv_protection and uv_protection == 'UV Protection':
+            include.append('UV Protection')
+
+        if parabens and parabens == 'no paraben':
+            exclude.append('Paraben')
+
+        if allergies and allergies == 'Allergens':
+            exclude.append('Allergens')
+
+        if skin_goals:
+            include.append(skin_goals)
+        
+        filter_by_clean = filter_by_feature(cleanser_df, include)
+        filter_out_clean = filter_out_features(cleanser_df, exclude)
+
+        filter_by_toner = filter_by_feature(toner_df, include)
+        filter_out_toner = filter_out_features(toner_df, exclude)
+
+        filter_by_serum = filter_by_feature(serums_df, include)
+        filter_out_serum = filter_out_features(serums_df, exclude)
+
+        filter_by_cream = filter_by_feature(moisturizers_df, include)
+        filter_out_cream = filter_out_features(moisturizers_df, exclude)
+
+        
+
+        
+
+
+if(__name__ == 'main'):
+    app.run()
